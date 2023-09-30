3e19c0f5ec1452bc6afed471c0767f2a298f7339
Jake-Purton <jakecapurton@gmail.com>
2023-09-30 15:47:19 +0100

    it works but is really bad (possibly colorblind

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@
 
 use std::fs;
 
-use candle_core::{DType, Result, Tensor, D, Device};
+use candle_core::{DType, Result, Tensor, D, Device, CudaDevice};
 use candle_nn::{loss, ops, Linear, Module, VarBuilder, VarMap, Optimizer};
 
 // giving the inputs all in one vector so input dim = 3 (r, g, b)
@@ -69,38 +69,37 @@ fn main() -> anyhow::Result<()> {
     
     // put the data in here
     let train_data: Vec<f32> = fs::read_to_string("training_data.txt")
-    .expect("Should have been able to read the file").split_ascii_whitespace().map(| a: &str | a.parse::<f32>().unwrap()/ 255.0).collect();
+        .expect("Should have been able to read the file").split_ascii_whitespace().map(| a: &str | a.parse::<f32>().unwrap()/ 255.0).collect();
 
     let train_data_tensor = Tensor::from_vec(train_data.clone(), (train_data.len() / INPUT_DIM, INPUT_DIM), &device)?.to_dtype(DType::F32)?;
 
     
-    let train_data_results: Vec<f32> = fs::read_to_string("training_data_answers.txt")
-    .expect("Should have been able to read the file").split_ascii_whitespace().map(| a: &str | a.parse::<f32>().unwrap() / 255.0).collect();
+    let train_data_results: Vec<u32> = fs::read_to_string("training_data_answers.txt")
+        .expect("Should have been able to read the file").split_ascii_whitespace().map(| a: &str | a.parse::<u32>().unwrap()).collect();
 
-    let train_data_results_tensor = Tensor::from_vec(train_data_results.clone(), train_data_results.len() / RESULTS, &device)?.to_dtype(DType::F32)?;
+    let train_results_tensor = Tensor::from_vec(train_data_results, train_data.len() / INPUT_DIM, &device)?;
 
     // test
 
     let test_data: Vec<f32> = fs::read_to_string("test_data.txt")
-    .expect("Should have been able to read the file").split_ascii_whitespace().map(| a: &str | a.parse::<f32>().unwrap() / 255.0).collect();
+        .expect("Should have been able to read the file").split_ascii_whitespace().map(| a: &str | a.parse::<f32>().unwrap() / 255.0).collect();
 
     let test_data_tensor = Tensor::from_vec(test_data.clone(), (test_data.len() / INPUT_DIM, INPUT_DIM), &device)?.to_dtype(DType::F32)?;
     
-    let test_data_results: Vec<f32> = fs::read_to_string("test_results.txt")
-    .expect("Should have been able to read the file").split_ascii_whitespace().map(| a: &str | a.parse::<f32>().unwrap() / 255.0).collect();
+    let test_data_results: Vec<u32> = fs::read_to_string("test_results.txt")
+        .expect("Should have been able to read the file").split_ascii_whitespace().map(| a: &str | a.parse::<u32>().unwrap()).collect();
 
-    let test_data_results_tensor = Tensor::from_vec(test_data_results.clone(), test_data_results.len() / RESULTS, &device)?.to_dtype(DType::F32)?;
+    let test_results_tensor = Tensor::from_vec(test_data_results.clone(), test_data_results.len(), &device)?;
     
     let m = Dataset {
         train_data_input: train_data_tensor,
-        train_data_results: train_data_results_tensor,
+        train_data_results: train_results_tensor,
         test_data: test_data_tensor,
-        test_data_results: test_data_results_tensor,
+        test_data_results: test_results_tensor,
     };
 
-    let trained_model: MultiLevelPerceptron;
+    let mut trained_model: MultiLevelPerceptron = MultiLevelPerceptron::new(VarBuilder::zeros(DType::F32, &device))?;
     loop {
-        println!("Trying to train neural network.");
         match train(m.clone(), &device) {
             Ok(model) => {
                 trained_model = model;
@@ -112,6 +111,27 @@ fn main() -> anyhow::Result<()> {
             }
         }
 
+    }
+
+    let purple: Vec<u32> = vec![
+        58, 51, 255,
+        102, 0, 204,
+        76, 0, 153,
+        255, 0, 0,
+        0, 0, 255,
+        0, 255, 0,
+    ];
+
+    let tensor_test = Tensor::from_vec(purple.clone(), (purple.len() / INPUT_DIM, INPUT_DIM), &device)?.to_dtype(DType::F32)?;
+
+    let final_result = trained_model.forward(&tensor_test)?;
+
+    let results = final_result
+        .argmax(D::Minus1)?
+        .to_dtype(DType::F32)?.to_vec1::<f32>()?;
+
+    for result in results {
+        println!("{result}")
     }
 
     // let final_result = trained_model.forward(&tensor_test_votes)?;
@@ -130,26 +150,30 @@ fn train(m: Dataset, dev: &Device) -> anyhow::Result<MultiLevelPerceptron> {
 
     // puts the training data in the gpu
     let train_results = m.train_data_results.to_device(dev)?;
+
+    
     let train_votes = m.train_data_input.to_device(dev)?;
     let varmap = VarMap::new();
     let vs = VarBuilder::from_varmap(&varmap, DType::F32, dev);
-
+    
     // makes the nn
     let model = MultiLevelPerceptron::new(vs.clone())?;
-
+    
     // Optimizer for Stochastic Gradient Descent.
     let mut sgd = candle_nn::SGD::new(varmap.all_vars(), LEARNING_RATE)?;
-
+    
     // puts the test data in
     let test_data = m.test_data.to_device(dev)?;
     let test_results = m.test_data_results.to_device(dev)?;
-
+    
     let mut final_accuracy: f32 = 0.0;
-
+    
     // trains it on the test data one time per epoch
     for epoch in 1..=EPOCHS {
         let logits = model.forward(&train_votes)?;
         let log_sm = ops::log_softmax(&logits, D::Minus1)?;
+
+        //somethinf doesn't work here
         let loss = loss::nll(&log_sm, &train_results)?;
         sgd.backward_step(&loss)?;
 
@@ -163,14 +187,14 @@ fn train(m: Dataset, dev: &Device) -> anyhow::Result<MultiLevelPerceptron> {
         let test_accuracy = sum_ok / test_results.dims1()? as f32;
         final_accuracy = 100. * test_accuracy;
         println!("Epoch: {epoch:3} Train loss: {:8.5} Test accuracy: {:5.2}%",
-                 loss.to_scalar::<f32>()?,
-                 final_accuracy
+                loss.to_scalar::<f32>()?,
+                final_accuracy
         );
         if final_accuracy == 100.0 {
             break;
         }
     }
-    if final_accuracy < 100.0 {
+    if final_accuracy < 80.0 {
         Err(anyhow::Error::msg("The model is not trained well enough."))
     } else {
         Ok(model)
