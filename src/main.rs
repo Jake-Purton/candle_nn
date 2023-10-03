7d892bc9bf8fa7b97902961b6585984a962b63c4
Jake-Purton <jakecapurton@gmail.com>
2023-10-03 23:02:41 +0100

    building from the ground up

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,60 @@
-use candle_core::{Device, Result, Tensor};
+mod train;
 
-struct Model {
-    first: Tensor,
-    second: Tensor,
+use candle_core::{Device, Result, Tensor};
+use candle_nn::{Linear, Module, VarBuilder};
+use train::train;
+use rust_mnist::Mnist;
+
+const LEARNING_RATE: f64 = 0.05;
+const EPOCHS: usize = 10;
+
+pub struct MultiLevelPerceptron {
+    ln1: Linear,
+    ln2: Linear,
+    ln3: Linear,
 }
 
-impl Model {
-    fn forward(&self, image: &Tensor) -> Result<Tensor> {
-        let x = image.matmul(&self.first)?;
-        let x = x.relu()?;
-        x.matmul(&self.second)
+impl MultiLevelPerceptron {
+    fn new(vs: VarBuilder) -> Result<Self> {
+
+        // 784 in
+        let ln1 = candle_nn::linear(784, 100, vs.pp("ln1"))?;
+        // 100 in
+        let ln2 = candle_nn::linear(100, 32, vs.pp("ln2"))?;
+        // 32 in
+        let ln3 = candle_nn::linear(32, 10, vs.pp("ln3"))?;
+        // 10 out
+        Ok(Self { ln1, ln2, ln3 })
     }
+
+    fn forward(&self, xs: &Tensor) -> Result<Tensor> {
+        let xs = self.ln1.forward(xs)?;
+        let xs = xs.relu()?;
+        let xs = self.ln2.forward(&xs)?;
+        let xs = xs.relu()?;
+        self.ln3.forward(&xs)
+    }
+}
+
+#[derive(Clone)]
+pub struct Dataset {
+    pub train_images: Tensor,
+    pub train_labels: Tensor,
+    pub test_images: Tensor,
+    pub test_labels: Tensor,
 }
 
 fn main() -> Result<()> {
     // Use Device::new_cuda(0)?; to use the GPU.
     let device = Device::cuda_if_available(0)?;
+    let mnist = Mnist::new("MNIST_data/");
+    
+    let train_images = mnist.train_data.clone()[0];
+    let train_labels = mnist.train_labels.clone()[0];
 
-    let first = Tensor::randn(0f32, 1.0, (784, 100), &device)?;
-    let second = Tensor::randn(0f32, 1.0, (100, 10), &device)?;
-    let model = Model { first, second };
+    println!("{}, {}", train_images.len(), train_labels);
 
-    let dummy_image = Tensor::randn(0f32, 1.0, (1, 784), &device)?;
+    // train(data, &device);
 
-    let digit = model.forward(&dummy_image)?;
-    println!("Digit {digit:?} digit");
     Ok(())
 }
---
+++ src/train.rs
@@ -0,0 +1,46 @@
+use candle_core::{Device, DType, D};
+use candle_nn::{VarMap, VarBuilder, Optimizer, ops, loss};
+
+use crate::{Dataset, MultiLevelPerceptron, LEARNING_RATE, EPOCHS};
+
+pub fn train(m: Dataset, dev: &Device) -> anyhow::Result<MultiLevelPerceptron> {
+    let train_labels = m.train_labels.to_device(dev)?;
+    let train_images = m.train_images.to_device(dev)?;
+    let varmap = VarMap::new();
+    let vs = VarBuilder::from_varmap(&varmap, DType::F32, dev);
+    let model = MultiLevelPerceptron::new(vs.clone())?;
+    let mut sgd = candle_nn::SGD::new(varmap.all_vars(), LEARNING_RATE)?;
+    let test_images = m.test_images.to_device(dev)?;
+    let test_labels = m.test_labels.to_device(dev)?;
+    let mut final_accuracy: f32 = 0.0;
+    for epoch in 1..EPOCHS + 1 {
+        let logits = model.forward(&train_images)?;
+
+        println!("{logits}, here jakey");
+
+        let log_sm = ops::log_softmax(&logits, D::Minus1)?;
+        let loss = loss::nll(&log_sm, &train_labels)?;
+        sgd.backward_step(&loss)?;
+
+        let test_logits = model.forward(&test_images)?;
+        let sum_ok = test_logits
+            .argmax(D::Minus1)?
+            .eq(&test_labels)?
+            .to_dtype(DType::F32)?
+            .sum_all()?
+            .to_scalar::<f32>()?;
+        let test_accuracy = sum_ok / test_labels.dims1()? as f32;
+        final_accuracy = 100. * test_accuracy;
+        println!("Epoch: {epoch:3} Train loss: {:8.5} Test accuracy: {:5.2}%",
+                loss.to_scalar::<f32>()?,
+                final_accuracy
+        );
+        if final_accuracy == 100.0 {
+            break;
+        }
+    }
+    println!("{final_accuracy}");
+
+    Ok(model)
+
+}
